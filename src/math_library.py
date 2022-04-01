6292eb6d85a21b6abe12b0e8a1b219b3eb8f8cc9
Miski <las.michal00@gmail.com>
2022-04-01 18:00:24 +0200

    Test math_lib_func branch

--- src/math_library.py
+++ src/math_library.py
@@ -1,25 +1,82 @@
-class MathFunctions:
-    # Definition of functions to prevent crashing of tests
-    def addition(self, num1: float, num2: float):
-        return False
 
+
+##
+# @file math_library.py
+# @brief Mathematical library for calculator
+# @author Michal Ľaš <xlasmi00@vutbr.cz>
+#
+
+
+class MathFunctions:
+
+    ##
+    # @brief function for adding 2 numbers
+    # @param num1 first number
+    # @param num2 second number
+    # @return addition of num1 and num2
+    #
+    def addition(self, num1: float, num2: float):
+        return round(num1 + num2, 10)
+
+    ##
+    # @brief finction for subtraction 
+    # @param
+    # @param
+    # @return
+    #
     def subtraction(self, num1: float, num2: float):
         return False
 
+    ##
+    # @brief
+    # @param
+    # @param
+    # @return
+    #
     def multiplication(self, num1: float, num2: float):
         return False
 
+    ##
+    # @brief
+    # @param
+    # @param
+    # @return
+    #
     def division(self, num1: float, num2: float):
         return False
 
+    ##
+    # @brief
+    # @param
+    # @param
+    # @return
+    #
     def factorial(self, num1: int):
         return False
 
+    ##
+    # @brief
+    # @param
+    # @param
+    # @return
+    #
     def power(self, num1: float, to_the_power: int):
         return False
 
+    ##
+    # @brief
+    # @param
+    # @param
+    # @return
+    #
     def root(self, num1: float, root: float):
         return False
 
+    ##
+    # @brief
+    # @param
+    # @param
+    # @return
+    #
     def modulo(self, num1: float, num2: float):
         return False

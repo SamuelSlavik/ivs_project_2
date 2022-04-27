ae65c314e9e43f3140ecae25c29cfe528ba63041
AdamPekny <peknyadam9@gmail.com>
2022-04-27 17:53:03 +0200

    Refactored profiling.py and updated profiling outputs.
    Refactored profiling.py:
    1, Added function parse_line which gets rid of all unnecessary characters
    2, Added function cnt_line_res which calculates needed values for each line
    Updated profiling outputs:
    1, Output update due to source code change

--- src/profiling.py
+++ src/profiling.py
@@ -2,28 +2,39 @@ import sys
 import re
 from math_library import MathFunctions
 
+
+def parse_line(_line):
+    _line = re.sub("\\s+", " ", _line.strip("\n"))
+
+    if _line[0] == " ":
+        _line = _line[1:]
+    if _line[-1] == " ":
+        _line = _line[:len(_line) - 1]
+
+    return _line
+
+
+def cnt_line_res(_data_cnt, _data_sum, _func_sum, _line):
+    for number in _line.split(" "):
+        number_flt = float(number)
+
+        _data_cnt = Math.addition(_data_cnt, 1)
+        _data_sum = Math.addition(_data_sum, number_flt)
+        _func_sum = Math.addition(_func_sum, Math.power(number_flt, 2))
+
+    return _data_cnt, _data_sum, _func_sum
+
+
 Math = MathFunctions()
 
 data_sum = 0
 data_avg = 0
 data_cnt = 0
 func_sum = 0
-data = []
 
 for line in sys.stdin:
-    line = re.sub("\\s+", " ", line.strip("\n"))
-
-    if line[0] == " ":
-        line = line[1:]
-    if line[-1] == " ":
-        line = line[:len(line) - 1]
-
-    for number in line.split(" "):
-        number_flt = float(number)
-
-        data_cnt = Math.addition(data_cnt, 1)
-        data_sum = Math.addition(data_sum, number_flt)
-        func_sum = Math.addition(func_sum, Math.power(number_flt, 2))
+    line = parse_line(line)
+    data_cnt, data_sum, func_sum = cnt_line_res(data_cnt, data_sum, func_sum, line)
 
 data_avg = Math.division(data_sum, data_cnt)
 
